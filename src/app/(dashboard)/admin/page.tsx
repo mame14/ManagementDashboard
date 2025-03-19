8ad457516cffa2b369a94bf8d95ab720de383d4e
mame14 <mame.6757@gmail.com>
2025-03-19 11:41:05 +0900

    "Navbar, Menu create"

---
+++ src/app/(dashboard)/admin/page.tsx
@@ -0,0 +1,12 @@
+const AdminPage = () => {
+  return (
+    <div className="p-4 flex gap-4 flex-col md:flex-row">
+      {/* LEFT */}
+      <div className="w-full lg:w-2/3">l</div>
+      {/* RIGHT */}
+      <div className="w-full lg:w-1/3">r</div>
+    </div>
+  );
+};
+
+export default AdminPage;
